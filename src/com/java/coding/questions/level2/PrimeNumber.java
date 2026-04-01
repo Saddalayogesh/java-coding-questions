c2741cb716b9cbcf472803602fd002dc300feeb8
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-01 23:35:34 +0530

    Update PrimeNumber.java

--- src/com/java/coding/questions/level2/PrimeNumber.java
+++ src/com/java/coding/questions/level2/PrimeNumber.java
@@ -7,11 +7,13 @@ public class PrimeNumber {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
         int num = sc.nextInt();
+
         boolean isPrime = true;
+
         if (num <= 1) {
             isPrime = false;
         } else {
-            for(int i = 2; i <= num / 2; ++i) {
+            for (int i = 2; i * i <= num; i++) {
                 if (num % i == 0) {
                     isPrime = false;
                     break;
