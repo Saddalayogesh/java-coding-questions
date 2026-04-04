1bebd68f24d34d58b72d3d29421748c6f5e1d0fb
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-04 23:08:45 +0530

    Update DigitalRoot.java

--- src/com/java/coding/questions/level4/DigitalRoot.java
+++ src/com/java/coding/questions/level4/DigitalRoot.java
@@ -1,19 +1,20 @@
-package com.java.coding.questions.level4;
-
-import java.util.Scanner;
-
 public class DigitalRoot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
 
-        int number;
-        int sum;
-        for(number = sc.nextInt(); number >= 10; number = sum) {
-            for(sum = 0; number > 0; number /= 10) {
-                int digit = number % 10;
-                sum += digit;
+        int number = sc.nextInt();
+
+        while (number >= 10) {
+            int sum = 0;
+            int temp = number;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
             }
+
+            number = sum;
         }
 
         System.out.println("Digital Root = " + number);
