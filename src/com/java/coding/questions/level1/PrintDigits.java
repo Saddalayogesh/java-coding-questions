7e3575b6f3e83292388cce8c5da60f9a219f5677
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-12 23:20:35 +0530

    Update PrintDigits.java

--- src/com/java/coding/questions/level1/PrintDigits.java
+++ src/com/java/coding/questions/level1/PrintDigits.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class PrintDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a  number: ");
+        System.out.print("Enter a number: ");
 
-        for(int num = sc.nextInt(); num != 0; num /= 10) {
-            int digit = num % 10;
-            System.out.println(digit);
+        int num = sc.nextInt();
+        int temp = num;
+        int reverse = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp /= 10;
         }
 
+        while (reverse != 0) {
+            int digit = reverse % 10;
+            System.out.println(digit);
+            reverse /= 10;
+        }
     }
 }
