5834b695e787a8e2a100f6647f9b4d73ea11badc
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-14 23:41:50 +0530

    Update PerfectNumber.java

--- src/com/java/coding/questions/level3/PerfectNumber.java
+++ src/com/java/coding/questions/level3/PerfectNumber.java
@@ -7,15 +7,20 @@ public class PerfectNumber {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
         int num = sc.nextInt();
-        int sum = 0;
 
-        for(int i = 1; i < num; ++i) {
-            if (num % i == 0) {
+        int sum = 1;
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
                 sum += i;
+
+                if(i != num / i) {
+                    sum += num / i;
+                }
             }
         }
 
-        if (sum == num) {
+        if(num != 1 && sum == num) {
             System.out.println("Perfect Number");
         } else {
             System.out.println("Not Perfect Number");
