47bde8770c3362928b1198ddb955296e70be2f40
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-17 23:19:30 +0530

    feat SquareSeries.java

--- src/com/java/coding/questions/level4/SquareSeries.java
+++ src/com/java/coding/questions/level4/SquareSeries.java
@@ -8,8 +8,13 @@ public class SquareSeries {
         System.out.print("Enter Number: ");
         int N = sc.nextInt();
 
-        for(int i = 1; i <= N; ++i) {
-            System.out.println(i * i);
+        if (N <= 0) {
+            System.out.println("Please enter a positive number.");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i * i + " ");
         }
 
         sc.close();
