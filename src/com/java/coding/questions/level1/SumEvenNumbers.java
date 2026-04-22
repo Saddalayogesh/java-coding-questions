72af4d70719b30fd44c787f33222a54407994d2b
Saddala Yogesh <saddalayogesh@gmail.com>
2026-04-22 23:52:57 +0530

    Update SumEvenNumbers.java

--- src/com/java/coding/questions/level1/SumEvenNumbers.java
+++ src/com/java/coding/questions/level1/SumEvenNumbers.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class SumEvenNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a  Number : ");
+
+        System.out.print("Enter a Number: ");
         int n = sc.nextInt();
+
         int sum = 0;
 
-        for(int i = 1; i <= n; ++i) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
+        for (int i = 2; i <= n; i += 2) {
+            sum += i;
         }
 
         System.out.println("Sum of even numbers = " + sum);
+
+        sc.close();
     }
 }
